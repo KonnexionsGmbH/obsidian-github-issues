e3648500b8131c26f361b03e32ca9f2288c3c628
Stefan Ochsenbein <ochsenbein@bluewin.ch>
2025-02-03 13:52:42 +0100

    Merge commit

--- src/Elements/Modals/NewIssueModal.ts
+++ src/Elements/Modals/NewIssueModal.ts
@@ -39,7 +39,7 @@ export class NewIssueModal extends Modal {
 			contentEl.appendChild(spinner);
 			//get the labels
 			// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-			const labels: = (
+			const labels = (
 				// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 				await api_get_labels(this.ocotoBundle.octokit, repo()!)
 			).map((label) => label.name);
